ef1c9c155b15b92307aef5bfa805cdd98308d080
Akhil Pillai <akhilpillai18@gmail.com>
2023-04-25 00:45:05 +0530

    Create server.js

---
+++ api/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/status', async (req, res) => {
+  try {
+    const url = req.query.url;
+    const response = await fetch(url);
+
+    if (response.ok) {
+      res.status(200).send(`${url} is up and running!`);
+    } else {
+      res.status(400).send(`${url} is down!`);
+    }
+  } catch (error) {
+    res.status(400).send(`Error: ${error.message}`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
