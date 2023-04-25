0721913f019075933324faf476784e5d7fb3b4b3
Akhil Pillai <akhilpillai18@gmail.com>
2023-04-25 14:57:01 +0530

    Update checkstatus.js

--- api/checkstatus.js
+++ api/checkstatus.js
@@ -1,13 +1,24 @@
-const axios = require("axios");
+const https = require('https');
+
+const checkStatus = (url) => {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      resolve(res.statusCode);
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+};
 
 module.exports = async (req, res) => {
-  const { url } = req.query;
-
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).send('Missing URL parameter');
+  }
   try {
-    const response = await axios.get(url);
-    const status = response.status;
-    res.json({ status });
-  } catch (error) {
-    res.json({ error: error.message });
+    const statusCode = await checkStatus(url);
+    res.status(200).send({ status: statusCode });
+  } catch (err) {
+    res.status(500).send('Error checking status');
   }
 };
